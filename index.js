9fdeb94ad0f8e56bcdd444f82c5d58d050559938
Henryk Wollik <olafyoorsen@hotmail.com>
2015-07-06 21:14:27 +0200

    index.js fixed wrong export

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = {
-    Mat3 = require('./Mat3'),
-    Mat4 = require('./Mat4'),
-    Vec2 = require('./Vec2'),
-    Vec3 = require('./Vec3'),
-    Vec4 = require('./Vec4'),
-    Quat = require('./Quat')
+    Mat3 : require('./Mat3'),
+    Mat4 : require('./Mat4'),
+    Vec2 : require('./Vec2'),
+    Vec3 : require('./Vec3'),
+    Vec4 : require('./Vec4'),
+    Quat : require('./Quat')
 };
