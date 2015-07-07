5ad70b7ff2223c82de0a7fa7e933264abb5760d3
Henryk Wollik <olafyoorsen@hotmail.com>
2015-07-07 16:18:48 +0100

    index.js added Utils

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ module.exports = {
     Vec2 : require('./Vec2'),
     Vec3 : require('./Vec3'),
     Vec4 : require('./Vec4'),
-    Quat : require('./Quat')
+    Quat : require('./Quat'),
+    Utils : require('./Utils')
 };
