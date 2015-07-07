81758984e213222e5f0e3ecc598fb6d4eab1c732
Henryk Wollik <olafyoorsen@hotmail.com>
2015-07-07 05:51:29 +0200

    Vec4 added fromVec3

--- Vec4.js
+++ Vec4.js
@@ -23,11 +23,19 @@ function set(a,b){
     a[3] = b[3];
     return a;
 }
+
 function set4(a,x,y,z,w){
     a[0] = x;
     a[1] = y;
     a[2] = z;
     a[3] = w;
+    return a;
+}
+
+function fromVec3(a,b){
+    a[0] = b[0];
+    a[1] = b[1];
+    a[2] = b[2];
     return a;
 }
 
@@ -46,6 +54,7 @@ var Vec4 = {
     create: create,
     set: set,
     set4: set4,
+    fromVec3 : fromVec3,
     copy: copy,
     equals: equals,
     equals4: equals4
