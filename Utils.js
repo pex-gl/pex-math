01722608c36704de906976008baa2b22147ae068
Henryk Wollik <olafyoorsen@hotmail.com>
2015-07-07 16:18:40 +0100

    Utils initial

---
+++ Utils.js
@@ -0,0 +1,61 @@
+function lerp(a,b,t){
+    return a + (b - a) * t;
+}
+
+function clamp(a,min,max){
+    return max === undefined ? Math.max(a,min) : Math.max(min,Math.min(a,max));
+}
+
+function smoothstep(min, max, x){
+    x = clamp((x - min)/(max - min), 0.0, 1.0);
+    return x * x * (3 - 2 * x);
+}
+
+function normalize(a,start,end){
+    return (a - start) / (end - start);
+}
+
+function map(a,inStart,inEnd,outStart,outEnd){
+    return outStart + (outEnd - outStart) * normalize(a,inStart,inEnd);
+}
+
+function toRadians(degrees){
+    return degrees * Math.PI / 180.0;
+}
+
+function toDegree(radians){
+    return radians * 180 / Math.PI;
+}
+
+function frac(a){
+    return a - Math.floor(a);
+}
+
+function sgn(a){
+    return a / Math.abs(a);
+}
+
+function isPOT(a){
+    return (a & (a - 1)) == 0;
+}
+
+var Utils = {
+    lerp : lerp,
+    clamp : clamp,
+    smoothstep : smoothstep,
+    normalize : normalize,
+    map       : map,
+    toRadians : toRadians,
+    toDegree  : toDegree,
+    frac : frac,
+    sgn  : sgn,
+    isPOT : isPOT
+};
+
+module.exports = Utils;
+
+
+
+
+
+
