f03960d76bf4931d9791bbd4c4f7a8608b374a22
Nick Nikolov <nick@nklv.me>
2017-11-02 14:08:57 +0000

    Fix standard errors in mat3

--- mat3.js
+++ mat3.js
@@ -52,15 +52,15 @@ function identity (a) {
 }
 
 function fromMat4 (a, b) {
-  a[ 0] = b[ 0]
-  a[ 1] = b[ 1]
-  a[ 2] = b[ 2]
-  a[ 3] = b[ 4]
-  a[ 4] = b[ 5]
-  a[ 5] = b[ 6]
-  a[ 6] = b[ 8]
-  a[ 7] = b[ 9]
-  a[ 8] = b[10]
+  a[0] = b[0]
+  a[1] = b[1]
+  a[2] = b[2]
+  a[3] = b[4]
+  a[4] = b[5]
+  a[5] = b[6]
+  a[6] = b[8]
+  a[7] = b[9]
+  a[8] = b[10]
   return a
 }
 
