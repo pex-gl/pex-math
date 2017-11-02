7b2fb2243cbfe3089ff5f16846f0eef6c59d5d3a
Nick Nikolov <nick@nklv.me>
2017-11-02 16:00:59 +0000

    Fix standard errors in bench-gl-matrix

--- benchmark/bench-gl-matrix.js
+++ benchmark/bench-gl-matrix.js
@@ -17,7 +17,6 @@ bench('mat4/translate', function (b) {
   b.end()
 })
 
-
 bench('mat4/rotate', function (b) {
   var mat4 = glMat4.create()
 
@@ -30,7 +29,7 @@ bench('mat4/lookAt', function (b) {
   var mat4 = glMat4.create()
 
   b.start()
-  glMat4.lookAt(mat4, [0, 4, 2], [0, 0, 0], [0, 1 ,0])
+  glMat4.lookAt(mat4, [0, 4, 2], [0, 0, 0], [0, 1, 0])
   b.end()
 })
 
