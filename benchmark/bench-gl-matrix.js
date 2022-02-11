44b1b443c9b34c53012f6b9f01cbcaa93e373bc8
Damien Seguin <dmnsgn@gmail.com>
2022-02-11 19:29:52 +0000

    test: add quat benchmarks

--- benchmark/bench-gl-matrix.js
+++ benchmark/bench-gl-matrix.js
@@ -1,13 +1,13 @@
 import bench from "nanobench";
-import { mat4 } from "gl-matrix";
+import { mat4, quat, mat3, vec3 } from "gl-matrix";
 import { run } from "./utils.js";
 
+// mat4
 bench("mat4/create", (b) => {
   b.start();
   run(() => mat4.create());
   b.end();
 });
-
 bench("mat4/identity", (b) => {
   const m = mat4.create();
 
@@ -15,15 +15,13 @@ bench("mat4/identity", (b) => {
   run(() => mat4.identity(m));
   b.end();
 });
-
-bench("mat4/clone", (b) => {
+bench("mat4/copy", (b) => {
   const m = mat4.create();
 
   b.start();
   run(() => mat4.clone(m));
   b.end();
 });
-
 bench("mat4/mult", (b) => {
   const m1 = mat4.create();
   const m2 = mat4.create();
@@ -32,7 +30,6 @@ bench("mat4/mult", (b) => {
   run(() => mat4.mul(m1, m1, m2));
   b.end();
 });
-
 bench("mat4/invert", (b) => {
   const m = mat4.create();
 
@@ -40,7 +37,6 @@ bench("mat4/invert", (b) => {
   run(() => mat4.invert(m, m));
   b.end();
 });
-
 bench("mat4/transpose", (b) => {
   const m = mat4.create();
 
@@ -48,7 +44,6 @@ bench("mat4/transpose", (b) => {
   run(() => mat4.transpose(m, m));
   b.end();
 });
-
 bench("mat4/transpose copy", (b) => {
   const out = mat4.create();
   const m = mat4.create();
@@ -57,7 +52,6 @@ bench("mat4/transpose copy", (b) => {
   run(() => mat4.transpose(out, m));
   b.end();
 });
-
 bench("mat4/scale", (b) => {
   const m = mat4.create();
 
@@ -65,7 +59,6 @@ bench("mat4/scale", (b) => {
   run(() => mat4.scale(m, m, [4, 2, 9]));
   b.end();
 });
-
 bench("mat4/translate", (b) => {
   const m = mat4.create();
 
@@ -73,7 +66,6 @@ bench("mat4/translate", (b) => {
   run(() => mat4.translate(m, m, [4, 2, 9]));
   b.end();
 });
-
 bench("mat4/rotate", (b) => {
   const m = mat4.create();
 
@@ -81,7 +73,6 @@ bench("mat4/rotate", (b) => {
   run(() => mat4.rotate(m, m, 0.42, [1, 0, 0]));
   b.end();
 });
-
 bench("mat4/fromQuat", (b) => {
   const m = mat4.create();
 
@@ -89,8 +80,7 @@ bench("mat4/fromQuat", (b) => {
   run(() => mat4.fromQuat(m, [0, 0, 0, 1]));
   b.end();
 });
-
-bench("mat4/fromRotationTranslationScale", (b) => {
+bench("mat4/fromTranslationRotationScale", (b) => {
   const m = mat4.create();
 
   b.start();
@@ -99,9 +89,7 @@ bench("mat4/fromRotationTranslationScale", (b) => {
   );
   b.end();
 });
-
-// bench("mat4/fromMat3", (b) => {});
-
+// bench("mat4/fromMat3", (b) => {}, (b) => {});
 bench("mat4/frustum", (b) => {
   const m = mat4.create();
 
@@ -109,7 +97,6 @@ bench("mat4/frustum", (b) => {
   run(() => mat4.frustum(m, -1, 1, -1, 1, -1, 1));
   b.end();
 });
-
 bench("mat4/perspective", (b) => {
   const m = mat4.create();
 
@@ -117,7 +104,6 @@ bench("mat4/perspective", (b) => {
   run(() => mat4.perspective(m, Math.PI * 0.5, 1, 0, 1));
   b.end();
 });
-
 bench("mat4/ortho", (b) => {
   const m = mat4.create();
 
@@ -125,11 +111,128 @@ bench("mat4/ortho", (b) => {
   run(() => mat4.perspective(m, -1, 1, -1, 1, -1, 1));
   b.end();
 });
-
 bench("mat4/lookAt", (b) => {
   const m = mat4.create();
 
   b.start();
   run(() => mat4.lookAt(m, [0, 4, 2], [0, 0, 0], [0, 1, 0]));
+  b.end();
+});
+
+// quat
+bench("quat/create", (b) => {
+  b.start();
+  run(() => quat.create());
+  b.end();
+});
+bench("quat/identity", (b) => {
+  const q = quat.create();
+
+  b.start();
+  run(() => quat.identity(q));
+  b.end();
+});
+bench("quat/copy", (b) => {
+  const q = quat.create();
+
+  b.start();
+  run(() => quat.clone(q));
+  b.end();
+});
+bench("quat/mult", (b) => {
+  const q1 = quat.create();
+  const q2 = quat.create();
+
+  b.start();
+  run(() => quat.multiply(q1, q1, q2));
+  b.end();
+});
+bench("quat/invert", (b) => {
+  const q = quat.create();
+
+  b.start();
+  run(() => quat.invert(q, q));
+  b.end();
+});
+bench("quat/conjugate", (b) => {
+  const q = quat.create();
+
+  b.start();
+  run(() => quat.conjugate(q, q));
+  b.end();
+});
+bench("quat/length", (b) => {
+  const q = quat.create();
+
+  b.start();
+  run(() => quat.length(q));
+  b.end();
+});
+bench("quat/normalize", (b) => {
+  const q = quat.create();
+
+  b.start();
+  run(() => quat.normalize(q, q));
+  b.end();
+});
+bench("quat/dot", (b) => {
+  const q1 = quat.create();
+  const q2 = quat.create();
+
+  b.start();
+  run(() => quat.dot(q1, q2));
+  b.end();
+});
+bench("quat/fromEuler", (b) => {
+  const q = quat.create();
+  const e = vec3.create();
+
+  b.start();
+  run(() => quat.fromEuler(q, e[0], e[1], e[2]));
+  b.end();
+});
+bench("quat/fromAxisAngle", (b) => {
+  const q = quat.create();
+  const v = [4, 2, 9];
+  const r = Math.PI;
+
+  b.start();
+  run(() => quat.setAxisAngle(q, v, r));
+  b.end();
+});
+bench("quat/fromAxes", (b) => {
+  const q = quat.create();
+  const x = [-1, 0, 0];
+  const y = [0, 1, 0];
+  const z = [0, 0, -1];
+
+  b.start();
+  run(() => quat.setAxes(q, x, y, z));
+  b.end();
+});
+bench("quat/fromMat3", (b) => {
+  const q = quat.create();
+  const m = mat3.create();
+
+  b.start();
+  run(() => quat.fromMat3(q, m));
+  b.end();
+});
+// bench("quat/fromMat4", (b) => {});
+bench("quat/fromTo", (b) => {
+  const q = quat.create();
+  const v = vec3.create();
+  const w = vec3.create();
+
+  b.start();
+  run(() => quat.rotationTo(q, v, w));
+  b.end();
+});
+bench("quat/slerp", (b) => {
+  const q1 = quat.create();
+  const q2 = quat.create();
+
+  b.start();
+  run(() => quat.slerp(q1, q1, q2, 0.5));
   b.end();
 });
--- benchmark/bench-pex.js
+++ benchmark/bench-pex.js
@@ -1,13 +1,13 @@
 import bench from "nanobench";
+import { euler, mat3, mat4, quat, vec3 } from "../index.js";
 import { run } from "./utils.js";
-import { mat3, mat4 } from "../index.js";
 
+// mat4
 bench("mat4/create", (b) => {
   b.start();
   run(() => mat4.create());
   b.end();
 });
-
 bench("mat4/identity", (b) => {
   const m = mat4.create();
 
@@ -15,26 +15,23 @@ bench("mat4/identity", (b) => {
   run(() => mat4.identity(m));
   b.end();
 });
-
-bench("mat4/clone", (b) => {
+bench("mat4/copy", (b) => {
   const m = mat4.create();
 
   b.start();
   run(() => mat4.copy(m));
   b.end();
 });
-
 bench("mat4/mult", (b) => {
-  // const m1 = mat4.create();
-  // const m2 = mat4.create();
-  const m1 = Float32Array.of(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
-  const m2 = Float32Array.of(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
+  const m1 = mat4.create();
+  const m2 = mat4.create();
+  // const m1 = Float32Array.of(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
+  // const m2 = Float32Array.of(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
 
   b.start();
   run(() => mat4.mult(m1, m2));
   b.end();
 });
-
 bench("mat4/invert", (b) => {
   const m = mat4.create();
 
@@ -42,7 +39,6 @@ bench("mat4/invert", (b) => {
   run(() => mat4.invert(m));
   b.end();
 });
-
 bench("mat4/transpose", (b) => {
   const m = mat4.create();
 
@@ -50,7 +46,6 @@ bench("mat4/transpose", (b) => {
   run(() => mat4.transpose(m));
   b.end();
 });
-
 bench("mat4/transpose copy", (b) => {
   const out = mat4.create();
   const m = mat4.create();
@@ -62,7 +57,6 @@ bench("mat4/transpose copy", (b) => {
   });
   b.end();
 });
-
 bench("mat4/scale", (b) => {
   const m = mat4.create();
 
@@ -70,7 +64,6 @@ bench("mat4/scale", (b) => {
   run(() => mat4.scale(m, [4, 2, 9]));
   b.end();
 });
-
 bench("mat4/translate", (b) => {
   const m = mat4.create();
 
@@ -78,7 +71,6 @@ bench("mat4/translate", (b) => {
   run(() => mat4.translate(m, [4, 2, 9]));
   b.end();
 });
-
 bench("mat4/rotate", (b) => {
   const m = mat4.create();
 
@@ -86,7 +78,6 @@ bench("mat4/rotate", (b) => {
   run(() => mat4.rotate(m, 0.42, [1, 0, 0]));
   b.end();
 });
-
 bench("mat4/fromQuat", (b) => {
   const m = mat4.create();
 
@@ -94,8 +85,7 @@ bench("mat4/fromQuat", (b) => {
   run(() => mat4.fromQuat(m, [0, 0, 0, 1]));
   b.end();
 });
-
-bench("mat4/fromRotationTranslationScale", (b) => {
+bench("mat4/fromTranslationRotationScale", (b) => {
   const m = mat4.create();
 
   b.start();
@@ -105,15 +95,14 @@ bench("mat4/fromRotationTranslationScale", (b) => {
   b.end();
 });
 
-bench("mat4/fromMat3", (b) => {
-  const m = mat4.create();
-  const m3 = mat3.create();
+// bench("mat4/fromMat3", (b) => {
+//   const m = mat4.create();
+//   const m3 = mat3.create();
 
-  b.start();
-  run(() => mat4.fromMat3(m, m3));
-  b.end();
-});
-
+//   b.start();
+//   run(() => mat4.fromMat3(m, m3));
+//   b.end();
+// });
 bench("mat4/frustum", (b) => {
   const m = mat4.create();
 
@@ -121,7 +110,6 @@ bench("mat4/frustum", (b) => {
   run(() => mat4.frustum(m, -1, 1, -1, 1, -1, 1));
   b.end();
 });
-
 bench("mat4/perspective", (b) => {
   const m = mat4.create();
 
@@ -129,7 +117,6 @@ bench("mat4/perspective", (b) => {
   run(() => mat4.perspective(m, Math.PI * 0.5, 1, 0, 1));
   b.end();
 });
-
 bench("mat4/ortho", (b) => {
   const m = mat4.create();
 
@@ -137,11 +124,135 @@ bench("mat4/ortho", (b) => {
   run(() => mat4.perspective(m, -1, 1, -1, 1, -1, 1));
   b.end();
 });
-
 bench("mat4/lookAt", (b) => {
   const m = mat4.create();
 
   b.start();
   run(() => mat4.lookAt(m, [0, 4, 2], [0, 0, 0], [0, 1, 0]));
+  b.end();
+});
+
+// quat
+bench("quat/create", (b) => {
+  b.start();
+  run(() => quat.create());
+  b.end();
+});
+bench("quat/identity", (b) => {
+  const q = quat.create();
+
+  b.start();
+  run(() => quat.identity(q));
+  b.end();
+});
+bench("quat/copy", (b) => {
+  const q = quat.create();
+
+  b.start();
+  run(() => quat.copy(q));
+  b.end();
+});
+bench("quat/mult", (b) => {
+  const q1 = quat.create();
+  const q2 = quat.create();
+
+  b.start();
+  run(() => quat.mult(q1, q2));
+  b.end();
+});
+bench("quat/invert", (b) => {
+  const q = quat.create();
+
+  b.start();
+  run(() => quat.invert(q));
+  b.end();
+});
+bench("quat/conjugate", (b) => {
+  const q = quat.create();
+
+  b.start();
+  run(() => quat.conjugate(q));
+  b.end();
+});
+bench("quat/length", (b) => {
+  const q = quat.create();
+
+  b.start();
+  run(() => quat.length(q));
+  b.end();
+});
+bench("quat/normalize", (b) => {
+  const q = quat.create();
+
+  b.start();
+  run(() => quat.normalize(q));
+  b.end();
+});
+bench("quat/dot", (b) => {
+  const q1 = quat.create();
+  const q2 = quat.create();
+
+  b.start();
+  run(() => quat.dot(q1, q2));
+  b.end();
+});
+bench("quat/fromEuler", (b) => {
+  const q = quat.create();
+  const e = euler.create();
+
+  b.start();
+  run(() => quat.fromEuler(q, e));
+  b.end();
+});
+bench("quat/fromAxisAngle", (b) => {
+  const q = quat.create();
+  const v = [4, 2, 9];
+  const r = Math.PI;
+
+  b.start();
+  run(() => quat.fromAxisAngle(q, v, r));
+  b.end();
+});
+bench("quat/fromAxes", (b) => {
+  const q = quat.create();
+  const x = [1, 0, 0];
+  const y = [0, 1, 0];
+  const z = [0, 0, -1];
+
+  b.start();
+  run(() => quat.fromAxes(q, x, y, z));
+  b.end();
+});
+bench("quat/fromMat3", (b) => {
+  const q = quat.create();
+  const m = mat3.create();
+
+  b.start();
+  run(() => quat.fromMat3(q, m));
+  b.end();
+});
+// bench("quat/fromMat4", (b) => {
+//   const q = quat.create();
+//   const m = mat4.create();
+
+//   b.start();
+//   run(() => quat.fromMat4(q, m));
+//   b.end();
+// });
+bench("quat/fromTo", (b) => {
+  const q = quat.create();
+  const v = vec3.create();
+  const w = vec3.create();
+
+  b.start();
+  run(() => quat.fromTo(q, v, w));
+  b.end();
+});
+bench("quat/slerp", (b) => {
+  const q1 = quat.create();
+  const q2 = quat.create();
+
+  b.start();
+  run(() => quat.slerp(q1, q2, 0.5));
   b.end();
 });
