78b4d092c2cea95906711cb92c1c315beb2bebdf
Damien Seguin <dmnsgn@gmail.com>
2024-02-13 13:49:26 +0000

    docs: update index jsdoc

--- index.js
+++ index.js
@@ -1,22 +1,79 @@
-/**
- * Re-export vec, avec, mat, quat, euler and utils
- * @module index
- */
+/** @module pex-math */
 
+/**
+ * @type {module:mat2x3}
+ * @name mat2x3
+ * @static
+ */
 export * as mat2x3 from "./mat2x3.js";
+/**
+ * @type {module:mat3}
+ * @name mat3
+ * @static
+ */
 export * as mat3 from "./mat3.js";
+/**
+ * @type {module:mat4}
+ * @name mat4
+ * @static
+ */
 export * as mat4 from "./mat4.js";
 
+/**
+ * @type {module:vec2}
+ * @name vec2
+ * @static
+ */
 export * as vec2 from "./vec2.js";
+/**
+ * @type {module:vec3}
+ * @name vec3
+ * @static
+ */
 export * as vec3 from "./vec3.js";
+/**
+ * @type {module:vec4}
+ * @name vec4
+ * @static
+ */
 export * as vec4 from "./vec4.js";
 
+/**
+ * @type {module:avec2}
+ * @name avec2
+ * @static
+ */
 export * as avec2 from "./avec2.js";
+/**
+ * @type {module:avec3}
+ * @name avec3
+ * @static
+ */
 export * as avec3 from "./avec3.js";
+/**
+ * @type {module:avec4}
+ * @name avec4
+ * @static
+ */
 export * as avec4 from "./avec4.js";
 
+/**
+ * @type {module:quat}
+ * @name quat
+ * @static
+ */
 export * as quat from "./quat.js";
 
+/**
+ * @type {module:euler}
+ * @name euler
+ * @static
+ */
 export * as euler from "./euler.js";
 
+/**
+ * @type {module:utils}
+ * @name utils
+ * @static
+ */
 export * as utils from "./utils.js";
